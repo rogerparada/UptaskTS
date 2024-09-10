981fbd9f0c5105bfbf02c2649de29010d5012961
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-09-10 19:27:25 +0200

    Add Cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRouter from "./routes/projectRoutes";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use("/api/projects", projectRouter);
 
